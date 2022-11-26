376ad78454054a9ab30a7da66f0ff8e8c7ee01d1
yugo uchiida <y.uchiida@nirneege.com>
2022-11-26 18:45:08 +0900

    blog 2: Article モデルを作成

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(default="", max_length=30)
+    text = models.TextField(
+        default="",
+    )
+    author = models.CharField(default="", max_length=30)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)

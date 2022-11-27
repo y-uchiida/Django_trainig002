d7400c1543a886750544057668108e2db3e5c13c
yugo uchiida <y.uchiida@nirneege.com>
2022-11-27 16:04:38 +0900

    blog 14: ログイン・ログアウトのリンクを表出

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.Login.as_view()),
-    path("logout/", LogoutView.as_view()),
-    path("signup", views.signup),
+    path("login/", views.Login.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup", views.signup, name="signup"),
 ]
